a1c87661fed37f9df4c0b5668cd58d20fee2276c
CoLorenzo <lorenzo2berardi@gmail.com>
2025-03-31 17:44:36 +0200

    Improved connection flexibility, allowing for a change of database address

--- connectors.py
+++ connectors.py
@@ -17,8 +17,9 @@ class PgVectorConnector:
         password = os.environ["POSTGRES_PASSWORD"]
         db = os.environ["POSTGRES_DB"]
         port = os.environ["POSTGRES_PORT"]
-        self.connection = f"postgresql+psycopg://{username}:{password}@localhost:{port}/{db}"
-    
+        address = os.environ.get("POSTGRES_ADDRESS", "localhost")
+        self.connection = f"postgresql+psycopg://{username}:{password}@{address}:{port}/{db}"
+
     def get_connection(self):
         return self.connection
     
